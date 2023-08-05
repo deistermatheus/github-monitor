86be9e203e88be887dea85d015ad900b51e35871
Matheus Deister Veiga <matheus.veiga@mundiale.com.br>
2023-08-05 11:46:52 -0300

    feat: list repos endpoint

--- repositories/views.py
+++ repositories/views.py
@@ -9,7 +9,7 @@ from common.services import GitHubService
 from repositories.tasks import capture_repository_commits_task
 
 
-@api_view(["GET"])
+@api_view(["GET",])
 @permission_classes([IsAuthenticated])
 def commit_list_view(request):
     commits = Commit.objects.all()
@@ -17,9 +17,7 @@ def commit_list_view(request):
     return Response(serializer.data)
 
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def repository_create_view(request):
+def repository_create(request):
     serializer = RepositorySerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
@@ -27,12 +25,27 @@ def repository_create_view(request):
     repository = serializer.validated_data['name']
 
     if not github_client.repository_exists(request.user, repository):
-        return Response("Repository does not exist in your Github Account.", status=status.HTTP_404_NOT_FOUND)
+        return Response("Repository does not exist in your GitHub account.", status=status.HTTP_404_NOT_FOUND)
 
     if Repository.objects.filter(name=repository).exists():
-        return Response("Repository is already added to Github monitor", status=status.HTTP_409_CONFLICT)
+        return Response("Repository is already added to GitHub monitor", status=status.HTTP_409_CONFLICT)
 
     serializer.save()
     capture_repository_commits_task.delay(str(request.user), repository)
 
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+def repository_list():
+    repositories = Repository.objects.all()
+    serializer = RepositorySerializer(repositories, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST", "GET"])
+@permission_classes([IsAuthenticated])
+def repository_view(request):
+    if request.method == "POST":
+        return repository_create(request)
+    if request.method == "GET":
+        return repository_list()
